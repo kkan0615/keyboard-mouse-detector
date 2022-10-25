30b886fce74d4bb7622f69a6b5f2462d92c5fa38
kkan0615 <kkan0615@gmail.com>
2022-10-24 21:20:50 -0400

    :zap: Feat: update preload

--- packages/preload/src/index.ts
+++ packages/preload/src/index.ts
@@ -7,7 +7,8 @@ contextBridge.exposeInMainWorld('versions', {
 })
 
 contextBridge.exposeInMainWorld('renderer', {
-  send: () => <T = any>(channel: string, listener: (event: IpcRendererEvent, args?: T) => void) => ipcRenderer.send(channel, listener),
+  send: <T = any>(channel: string, args?: T) => ipcRenderer.send(channel, args),
   on: <T = any>(channel: string, listener: (event: IpcRendererEvent, args?: T) => void) => ipcRenderer.on(channel, listener),
   off: () => (channel: string, listener: (event: IpcRendererEvent) => void) => ipcRenderer.off(channel, listener),
+  invoke: async <T = any>(channel: string, args?: T): Promise<any> => await ipcRenderer.invoke(channel, args),
 })
