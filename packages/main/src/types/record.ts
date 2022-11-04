e2bbed41c71a6b3e8b9c57f7d50aa4de94559139
kkan0615 <kkan0615@gmail.com>
2022-11-04 12:35:21 -0400

    :zap: Feat: add event factory util

--- packages/main/src/types/record.ts
+++ packages/main/src/types/record.ts
@@ -2,26 +2,37 @@ import dayjs from 'dayjs'
 import { UiohookKeyboardEvent, UiohookMouseEvent, UiohookWheelEvent } from 'uiohook-napi'
 import { iohookValue, ResHookKeyboardEvent, ResHookMouseEvent, ResHookWheelEvent } from './hookEvent'
 
-const parseKeyboardEvent = (e: UiohookKeyboardEvent) => {
-  return {
-    ...e,
-    keyName: iohookValue[e.keycode],
-    time: dayjs().toISOString(),
-  } as ResHookKeyboardEvent
-}
+/**
+ * Parse event data factory
+ * @param type
+ * @param e - event
+ */
+const eventFactory = (type: 'keyboard' | 'mouse' | 'wheel',
+  e: UiohookKeyboardEvent | UiohookMouseEvent | UiohookWheelEvent) => {
+  // Current time
+  const time = dayjs().toISOString()
 
-const parseMouseEvent = (e: UiohookMouseEvent) => {
-  return {
-    ...e,
-    time: dayjs().toISOString(),
-  } as ResHookMouseEvent
-}
-
-const parseWheelEvent = (e: UiohookWheelEvent) => {
-  return {
-    ...e,
-    time: dayjs().toISOString(),
-  } as ResHookWheelEvent
+  switch (type) {
+    case 'keyboard': {
+      return {
+        ...e,
+        keyName: iohookValue[(e as UiohookKeyboardEvent).keycode],
+        time,
+      } as ResHookKeyboardEvent
+    }
+    case 'mouse': {
+      return {
+        ...e,
+        time,
+      } as ResHookMouseEvent
+    }
+    case 'wheel': {
+      return {
+        ...e,
+        time,
+      } as ResHookWheelEvent
+    }
+  }
 }
 
 /**
@@ -80,7 +91,7 @@ export class Record {
    * @param event - Keyboard event
    */
   public addKeyboardEvent(event: UiohookKeyboardEvent) {
-    const parseEvent = parseKeyboardEvent(event)
+    const parseEvent = eventFactory('keyboard', event)
     this.events.push(parseEvent)
 
     return parseEvent
@@ -91,7 +102,7 @@ export class Record {
    * @param event - Mouse event
    */
   public addMouseEvent(event: UiohookMouseEvent) {
-    const parseEvent = parseMouseEvent(event)
+    const parseEvent = eventFactory('mouse', event)
     this.events.push(parseEvent)
 
     return parseEvent
@@ -102,7 +113,7 @@ export class Record {
    * @param event - Wheel event
    */
   public addWheelEvent(event: UiohookWheelEvent) {
-    const parseEvent = parseWheelEvent(event)
+    const parseEvent = eventFactory('wheel', event)
     this.events.push(parseEvent)
 
     return parseEvent
