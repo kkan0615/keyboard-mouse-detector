e6c2e92e8114673acba5e43ade34e4c97560b858
kkan0615 <kkan0615@gmail.com>
2022-11-11 16:02:14 -0500

    :zap: Feat: ad ko dictionary

--- packages/renderer/src/locales/langs/ko.ts
+++ packages/renderer/src/locales/langs/ko.ts
@@ -1,3 +1,71 @@
 export const koDictionary = {
-  test: '안녕하세요 !',
+  test: '안녕하세요',
+  commons: {
+    tooltips: {
+      search: '검색',
+      edit: '수정',
+      delete: '삭제',
+      ok: '확인',
+      save: '저장',
+    },
+    buttons: {
+      create: '생성',
+      save: '저장',
+      cancel: '취소',
+      edit: '수정',
+      delete: '삭제',
+      search: '검색',
+      send: '보내기',
+      copyURL: 'URL 복사',
+      open: '열기',
+      change: '변경',
+      stop: '중지'
+    },
+    Messages: {
+      cancelSave: 'Would you like to cancel save?',
+      saved: 'Success to saved',
+      saveFailed: 'Fail to save',
+      unAuthUser: 'unauthorized user',
+      error: 'Face to error',
+      noImg: 'No Image',
+      notMatched: 'It is not matched',
+      noData: 'no data',
+      Validations: {
+        required: '{field} field is required',
+        lengthMin: '{length} is minimum',
+        lengthMax: '{length} is maximum',
+        integer: 'Type number more than 0',
+        notMatched: '{field1} is not matched with {field2}',
+        passwordRule1: 'Password must include special character',
+        passwordRule2: 'Password must include at least one capital letter',
+      },
+      Questions: {
+        delete: 'Would you like to delete?',
+        getOffWork: 'would you like to get off {name}'
+      }
+    },
+    titles: {
+      cancel: '취소',
+      delete: '삭제',
+      total: '총',
+      attachment: '첨부파일',
+      comment: '댓글',
+    },
+    placeholders: {
+      noData: '데이터 없음',
+    },
+  },
+  types: {
+  },
+  pages: {
+    home: {
+      title: 'Detector',
+      setting: {
+        title: '설정',
+        downloadPath: '다운로드 경로',
+        events: '이벤트',
+        eventsSubMsg: '이벤트를 선택해주세요.',
+      }
+    },
+  }
 }
